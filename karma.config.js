90a2097124fa0630d257844d84d2d1eef38eb240
Kien Nguyen <kien.nguyen@go1.com.au>
2015-01-17 12:30:14 +0700

    Add Karma configs

---
+++ karma.config.js
@@ -0,0 +1,23 @@
+module.exports = function ( karma ) {
+  karma.set({
+    basePath: './',
+    files: [
+      <% scripts.forEach( function ( file ) { %>'<%= file %>',
+      <% }); %>
+      'src/**/*.js',
+    ],
+    exclude: [
+    ],
+    frameworks: [ 'jasmine' ],
+    plugins: [ 'karma-jasmine', 'karma-firefox-launcher' ],
+    reporters: 'dots',
+    port: 9018,
+    runnerPort: 9100,
+    urlRoot: '/',
+    autoWatch: false,
+    browsers: [
+      'Firefox'
+    ]
+  });
+};
+
