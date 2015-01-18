433c9dcb146f895fa39a60eecea1f456124b0822
Kien Nguyen <kien.nguyen@go1.com.au>
2015-01-18 19:18:13 +0700

    tdd 3 update code to allow the Add method to handle new lines between numbers (instead of commas)

--- src/calculator.js
+++ src/calculator.js
@@ -2,7 +2,7 @@ var Calculator = (function () {
   var add = function (numbers) {
     var total = 0;
     if (numbers) {
-      var numbersArray = numbers.split(',');
+      var numbersArray = numbers.split(/[\n,]/);
       for (var i = 0; i < numbersArray.length; i++) {
         total += parseInt(numbersArray[i]);
       }
