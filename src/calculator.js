74def9b6524a3ca202517767976ad1a31cd947ef
Kien Nguyen <kien.nguyen@go1.com.au>
2015-01-19 00:44:53 +0700

    tdd 4 update code to support different delimiter

--- src/calculator.js
+++ src/calculator.js
@@ -1,8 +1,23 @@
 var Calculator = (function () {
   var add = function (numbers) {
-    var total = 0;
+    var total = 0,
+      delimiter = ',',
+      delimiterArray = [],
+      pattern = '',
+      numbersArray = [];
+
     if (numbers) {
-      var numbersArray = numbers.split(/[\n,]/);
+      if(delimiterArray = numbers.match(/\/\/(.)*\n/)) {
+        delimiter = delimiterArray[1];
+        numbers = numbers.split('\n')[1];
+        pattern = '[' + delimiter + ']';
+      }
+      else {
+        pattern = '[\n,]';
+      }
+
+      pattern = new RegExp(pattern);
+      numbersArray = numbers.split(pattern);
       for (var i = 0; i < numbersArray.length; i++) {
         total += parseInt(numbersArray[i]);
       }
