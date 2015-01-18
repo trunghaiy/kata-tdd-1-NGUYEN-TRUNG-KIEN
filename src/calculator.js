a08bf9d24860b238dfaca09a611dbe586c08297b
Kien Nguyen <kien.nguyen@go1.com.au>
2015-01-18 12:29:04 +0700

    tdd 1.1 create method Add

---
+++ src/calculator.js
@@ -0,0 +1,19 @@
+var Calculator = (function () {
+  var add = function (numbers) {
+    var total = 0;
+    if (numbers) {
+      var numbersArray = numbers.split(',');
+      if (numbersArray.length > 2) {
+        return;
+      }
+      for (var i = 0; i < numbersArray.length; i++) {
+        total += parseInt(numbersArray[i]);
+      }
+    }
+    return total;
+  };
+
+  return {
+    add: add
+  }
+})();
